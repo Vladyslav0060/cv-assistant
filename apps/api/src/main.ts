8b0484ba3675de0b9fb96dfae51d1e6ba3e5db70
Vladyslav Bortnik <77225402+Vladyslav0060@users.noreply.github.com>
2026-02-26 17:59:16 +0200

    prisma init

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 5050);
+  console.log('URL: ', process.env.DIRECT_URL);
 }
 bootstrap();
